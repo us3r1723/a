62093ae87443bef39707dae656063da467667f27
us3r1723 <83931551+us3r1723@users.noreply.github.com>
2023-11-24 05:12:48 -0300

    Delete carros-completo directory

--- carros-completo/js/base.js
+++
@@ -1,8 +0,0 @@
- // O método filter é usado para criar um novo array com todos os elementos que passam em um teste implementado pela função fornecida.
- const carrosCor = carros.filter(carro => carro.cor.toLowerCase() === 'vermelho');
- console.log(carrosCor);
-
- // O método map é usado para criar um novo array baseado nos resultados da aplicação de uma função a cada elemento do array original.
-
- const precosComDesconto = carros.map(carro => carro.preco * .5);
- console.log(precosComDesconto);
--- carros-completo/js/somarEstoque.js
+++
@@ -1,4 +0,0 @@
-function somarEstoque(carros) { 
-    const total = carros.reduce((acumulador, carro) => acumulador + carro.preco, 0);
-    return total;
-}
